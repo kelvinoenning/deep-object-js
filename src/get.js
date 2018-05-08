bcf2c63b86fae52fed33c378a6bc2d93d5d08714
kelvinoenning <kelvinstang@hotmail.com>
2018-05-08 08:34:41 -0300

    return undefined when error

--- src/get.js
+++ src/get.js
@@ -1,12 +1,16 @@
 const _mountPathList = (path) => {
-    if(typeof path !== 'string') throw new Error('path is not a string')
+    if (typeof path !== 'string') throw new Error('path is not a string')
     return path.replace(/\[(\d+)]/g, '.$1').split('.')
 };
 
 const get = (obj, path, def) => {
-    if (typeof obj !== 'object') throw new Error('object is not accepted')
-    let pathList = _mountPathList(path);
-    return (pathList.every(step => (obj = obj[step]) !== undefined)) ? obj : def;
+    try {
+        if (typeof obj !== 'object') throw new Error('object is not accepted')
+        let pathList = _mountPathList(path);
+        return (pathList.every(step => (obj = obj[step]) !== undefined)) ? obj : def;
+    } catch (err) {
+        return undefined
+    }
 }
 
 module.exports = {
